f4eef203f6176dc634b0a06f878d11657a14b53d
saul <saulalonsopalazuelos@outlook.com>
2020-08-26 20:55:29 -0600

    stompjs2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var Stomp = require('stompjs');
+const SockJS = require('sockjs-client');
+
+var stompClient = null;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log('constructed')
+  }
+
+  connect() {
+    var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, (frame) => {
+      console.log('Connected: ' + frame);
+      stompClient.subscribe('/topic/greetings', (greeting) => {
+        console.log('recived : ', JSON.parse(greeting.body).content);
+      });
+    });
+  }
+
+  componentDidMount() {
+    console.log('initialized')
+    this.connect();
+  }
+
+  componentWillUnmount() {
+    if (stompClient !== null) {
+      stompClient.disconnect();
+      console.log("disconnected")
+    }
+  }
+
+  disconnect() {
+    if (stompClient !== null) {
+      stompClient.disconnect();
+    }
+    console.log("Disconnected");
+  }
+
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary" disableElevation
+          onClick={() => { stompClient.send("/app/hello", {}, JSON.stringify({ 'name': "Optimus" })); }}>Hello World</Button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
